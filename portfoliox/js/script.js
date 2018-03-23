7fc68136628694d96c1a708d08f4971e8a3a2a53
LuisCarrillo21 <luiz.karrillo@hotmail.com>
2018-03-23 03:49:39 -0700

    new advances

--- portfoliox/js/script.js
+++ portfoliox/js/script.js
@@ -3,3 +3,13 @@ function reverseColor()
 document.getElementById('background').style.backgroundColor = "yellow";
 document.getElementById('description').style.color = "blue";
 }
+
+
+function contactInf()
+{
+    document.write='Hello';
+}
+
+// var canvas =
+// ctx =canvas.getContext('ct')
+// ctx.fillText(10,30)
