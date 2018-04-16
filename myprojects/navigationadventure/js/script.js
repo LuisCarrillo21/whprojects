4eb0f95072d4a43aa06c04dde6c81b23440c2bab
LuisCarrillo21 <luiz.karrillo@hotmail.com>
2018-04-16 13:52:01 -0700

    Prjects Update

---
+++ myprojects/navigationadventure/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+// function askforName() {
+//     var userName = prompt("What is your name?");
+// }
+
+// document.getElementById('nopopup').onclick = function askforName() { 
+//         var userName = prompt("What is your name?");
+//       else { return false; }
+// };
+
+// var colors = ['#ff0000', '#00ff00', '#0000ff'];
+// var random_color = colors[Math.floor(Math.random() * colors.length)];
+// document.getElementById('rou').style.color = random_color;
